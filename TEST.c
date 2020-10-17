49f8309ef9b13d5e364094c8d7fa4ebb0394f1a5
Sunny Chaudhary <51516961+UndefeatedSunny@users.noreply.github.com>
2020-10-18 02:55:02 +0530

    Update TEST.c

--- TEST.c
+++ TEST.c
@@ -5,6 +5,16 @@
 
 #define ll long long int
 
+struct record                                   // Declare a Structure used to strore or write Data.
+{
+    char date[15];
+    char day[10];
+    char time[10];
+    char note[500];
+};
+
+
+
 int password()
 {
     char pass[15]={0};                          // For taking i/p (password) from User for verification Purpose.
@@ -12,10 +22,10 @@ int password()
     char ch;
     int j=0,i=0;
 
-    printf("PLEASE! ENTER THE PASSWORD\n");
-    printf("ONLY 3 CHANCES WILL BE GIVEN\n");
+    printf("\t\tPLEASE! ENTER THE PASSWORD\n\n");
+    printf("\t\tONLY 3 CHANCES WILL BE GIVEN\n");
 
-    for(i=0;i<=2;i++)                       // Loop Run 3 Times(Maximum).
+    for(i=0;i<=2;i++)                           // Loop Run 3 Times(Maximum).
     {
         j=0;
         printf("\n\n\nEnter the Password-> ");
@@ -63,7 +73,7 @@ int password()
                 j++;
             }
         }
-        if(strcmp(pass,check)==0)           // Compare the User Password with Original Password.
+        if(strcmp(pass,check)==0)               // Compare the User Password with Original Password.
         {
             printf("\n\t\t\tACCESS GRANTED \n");
             return 0;
@@ -79,6 +89,9 @@ int password()
     return 1;
 }
 
+
+
+
 void edit_password()
 {
     system("cls");                                                 // Clear the Screen.
@@ -88,7 +101,7 @@ void edit_password()
     int match,i=0,choice;
 
     printf("\n\n");
-    FILE *fp;                                                       // File Pointer to password file
+    FILE *fp;                                                       // File Pointer to password file.
     fp=fopen("password","rb");                                      // Read Binary Mode
     if(fp==NULL)
     {
@@ -186,6 +199,164 @@ void edit_password()
     getch();
 }
 
+void add_record()
+{
+    system("cls");                                                  // For Clearing Console.
+    if(password()!=0)                                               // Password Protection. (-_-)
+    {
+        return;
+    }
+    char enter='Y';
+    int choice;
+    FILE *fp;
+    struct record add;
+    char date[15];                                                  // For Creating Filename (Name is SAME as Date).
+    char time[15];                                                  // Whether Note already Exists or not (Based on Time).
+    printf("\n\n\t\t\t***************************\n");
+    printf("\t\t\t* WELCOME TO THE ADD MENU *");
+    printf("\n\t\t\t***************************\n\n");
+    printf("\n\n\t\tENTER DATE OF YOUR RECORD:[yyyy-mm-dd]: ");
+    fflush(stdin);
+    gets(date);
+    fp=fopen(date,"ab+");                   // Append Data + ((Read + Write) Permissions), only if file is present.
+    if(fp==NULL)                            // Means File is NOT Present.
+    {
+        fp=fopen(date,"wb+");               // Creation of FILE if not present on that DATE.
+        if(fp==NULL)
+        {
+            printf("\nSYSTEM ERROR -_-");
+            printf("\nEnter any KEY to exit");
+            getch();
+            return;
+        }
+    }
+    while(enter=='Y'||enter=='y')
+    {
+        choice=0;
+        fflush(stdin);
+        printf("\n\t\tENTER TIME:[hh:mm]: ");    //Whether Note already Exists or not (Based on Time).
+        scanf("%s",time);
+        rewind(fp);                          // Back pointer to the Initial Position For READ Data.
+        while(fread(&add,sizeof(add),1,fp)==1)
+        {
+            if(strcmp(add.time,time)==0)
+            {
+                printf("\n Already Exists");
+                choice=1;
+            }
+        }
+        if(choice==0)
+        {
+            strcpy(add.time,time);
+            printf("\n\t\tEnter the Date: ");
+            fflush(stdin);
+            gets(add.date);
+            fflush(stdin);
+            printf("\n\t\tEnter the Day: ");
+            gets(add.day);
+            fflush(stdin);
+            printf("\n\t\tStart Type the NOTE: ");
+            gets(add.note);
+            fwrite(&add, sizeof(add),1,fp) ;
+            printf("\n\t\tYOUR RECORD IS ADDED...\n");
+        }
+        printf("\n\t ADD another NOTE .... (Y/N) ");
+        fflush(stdin);
+        enter=getch();                       // Checking whether user wanted another note on same day.
+    }
+    fclose(fp);
+    printf("Enter any KEY to Exit");
+    getch();
+
+}
+
+
+
+void view_record()
+{
+    system("cls");
+    FILE *fp;
+    struct record owner;
+    char date_verif[15];
+    char time[10];
+    int option;
+    char choice;
+    printf("\n\n\t\t*******************************\n");
+    printf("\t\t* HERE IS THE VIEWING MENU *");
+    printf("\n\t\t*******************************\n\n");
+    if(password()!=0)
+    {
+        return;
+    }
+
+    do  // RUN 1 time minimum (Thats what we need for Verification Purpose).
+    {
+        printf("\n\t\tENTER THE DATE OF RECORD TO BE VIEWED:[yyyy-mm-dd]: ");
+        fflush(stdin);
+        gets(date_verif);
+
+        fp=fopen(date_verif,"rb");
+        if(fp==NULL)                            // Checking Whether Record is Present or NOT.
+        {
+            printf("THE RECORD NOT EXISTS -_-\nPlease Enter any KEY to EXIT\n");
+            getch();
+            return;
+        }
+        system("cls");
+        printf("\n\tPLEASE CHOOSE THE VIEW TYPE (-_-):\n");
+        printf("\n\t1.RECORD OF WHOLE DAY.");
+        printf("\n\t2.RECORD OF PARTICULAR TIME.");
+        printf("\n\n\tPLEASE ENTER YOUR CHOICE: ");
+        scanf("%d",&option);
+
+        switch(option)
+        {
+            case 1:
+                printf("\n\t\tRECORD OF WHOLE DAY [%s] IS -->> \n",date_verif);
+                while(fread(&owner,sizeof(owner),1,fp)==1)
+                {
+                    printf("\nDATE : \n\t%s",owner.date);
+                    printf("\nTIME : \n\t%s",owner.time);
+                    printf("\nDAY : \n\t%s",owner.day);
+                    printf("\nNOTE : \n\t%s",owner.note);
+                    printf("\n");
+                }
+                break;
+
+            case 2:
+                fflush(stdin);
+                printf("\nENTER TIME:[hh:mm]:");
+                gets(time);
+                while(fread(&owner,sizeof(owner),1,fp)==1)
+                {
+                    if(strcmp(time,owner.time)==0)
+                    {
+                        printf("\nHERE IS YOUR RECORD SIR (-_-)");
+                        printf("\n\nDATE : \n\t%s",owner.date);
+                        printf("\nTIME : \n\t%s",owner.time);
+                        printf("\nDAY : \n\t%s",owner.day);
+                        printf("\nNOTE : \n\t%s",owner.note);
+                        printf("\n");
+                    }
+                }
+                break;
+
+            default:
+                printf("\nPLEASE ENTER THE CORRECTION OPTION");
+                break;
+        }
+        printf("\n\nWOULD YOU LIKE TO CONTINUE VIEWING...(Y/N):");
+        fflush(stdin);
+        scanf("%c",&choice);
+    }
+    while(choice=='Y'||choice=='y');
+
+    fclose(fp);
+    return;
+
+}
+
+
 int main()
 {
     int option;
@@ -203,16 +374,16 @@ int main()
         printf("\n\t\t\tDELETE RECORD\t\t[3]");
         printf("\n\t\t\tEDIT PASSWORD\t\t[4]");
         printf("\n\t\t\tEXIT\t\t\t[5]");
-        printf("\n\n\t\t\tPlease! Enter the Option :");
+        printf("\n\n\t\t\tPlease! Enter the Option: ");
         scanf("%d",&option);
 
         switch(option)
         {
             case 1:
-//                add_record();
+                add_record();
                 break;
             case 2:
-   //             view_record();
+                view_record();
                 break;
             case 3:
      //           delete_record();
@@ -221,8 +392,9 @@ int main()
                 edit_password();
                 break;
             case 5:
-                printf("THANK YOU");
+                printf("\n\t\t\tTHANK YOU\t [-_-]\n");
                 getch();
+                return 0;
                 break;
         }
         system("cls");
